a55e508014a5fe20624762b8c4d93d1fb8b4c2b2
shreesha00 <shreeshagbhat@gmail.com>
2024-02-26 22:16:23 -0700

    Added some new files

---
+++ lazylog-benchmarking/scripts/append_bug.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/scalog/scalog/client"
+	log "github.com/scalog/scalog/logger"
+	"github.com/scalog/scalog/pkg/address"
+	"github.com/spf13/viper"
+)
+
+const NumberOfRequest = 10
+const NumberOfBytes = 100
+const StreamName = "AppendBenchmark"
+
+func main() {
+	var err error
+	// clean up old files
+	err = os.RemoveAll("log")
+	if err != nil {
+		log.Errorf("%v", err)
+	}
+	// read configuration file
+	viper.SetConfigFile("../../.scalog.yaml")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Errorf("read config file error: %v", err)
+	}
+
+	numReplica := int32(viper.GetInt("data-replication-factor"))
+	discPort := uint16(viper.GetInt("disc-port"))
+	discIp := viper.GetString(fmt.Sprintf("disc-ip"))
+	discAddr := address.NewGeneralDiscAddr(discIp, discPort)
+	dataPort := uint16(viper.GetInt("data-port"))
+	dataAddr := address.NewGeneralDataAddr("data-%v-%v-ip", numReplica, dataPort)
+
+	cli, err := client.NewClient(dataAddr, discAddr, numReplica)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+	}
+
+	for numberOfRequest := 0; numberOfRequest < 1002; numberOfRequest++ {
+		record := strconv.Itoa(numberOfRequest)
+		gsn, _, err := cli.AppendOne(record)
+
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		fmt.Println("executing ", record, " ", gsn)
+	}
+	test, err := cli.Read(1001, 0, 0)
+	if err != nil {
+		fmt.Println("read error")
+	}
+	if test != "1001" {
+		fmt.Println("bug!! expected 1001, got ", test)
+	}
+}
