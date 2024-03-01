ebf4dd9badec8084762ee4b4a204bdbe782a5862
shreesha00 <shreeshagbhat@gmail.com>
2024-03-01 15:29:55 -0700

    Sync

---
+++ lazylog-benchmarking/scripts/init_disk.sh
@@ -0,0 +1,3 @@
+sudo mkdir /users/sgbhat3/scalog-storage
+sudo /usr/local/etc/emulab/mkextrafs.pl -f /users/sgbhat3/scalog-storage
+sudo chown -R sgbhat3 /users/sgbhat3/scalog-storage
---
+++ lazylog-benchmarking/scripts/run_script_on_servers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+remote_nodes=("159" "038" "036" "124" "123" "126" "039")
+PASSLESS_ENTRY="/users/sgbhat3/.ssh/id_rsa"
+
+# Check if the local script path is provided as a command line argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <local_script_path>"
+    exit 1
+fi
+
+local_script="$1"
+
+# Iterate over remote nodes and execute the script
+for node in "${remote_nodes[@]}"; do
+    echo "Executing script on $node..."
+    ssh -i ${PASSLESS_ENTRY} "sgbhat3@hp$node.utah.cloudlab.us" "sudo bash -s" < "$local_script" &
+done
+
+wait 
+echo "Done"
