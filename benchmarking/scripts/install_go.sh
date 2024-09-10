90b7720e77c4da7c644f62cb0555e5886043819e
Shreesha G Bhat <shreeshagbhat@gmail.com>
2024-09-10 17:38:17 -0500

    Changed install_go.sh to check arch version

--- benchmarking/scripts/install_go.sh
+++ benchmarking/scripts/install_go.sh
@@ -3,10 +3,21 @@
 cd ~
 sudo apt update
 
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "aarch64" ]; then
+  GO_TAR="go1.22.0.linux-arm64.tar.gz"
+elif [ "$ARCH" = "x86_64" ]; then
+  GO_TAR="go1.22.0.linux-amd64.tar.gz"
+else
+  echo "unsupported architecture: $ARCH"
+  exit 1
+fi
+
 # install go
-wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
-sudo rm -rf go1.22.0.linux-amd64.tar.gz
+wget https://go.dev/dl/$GO_TAR
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $GO_TAR
+sudo rm -rf $GO_TAR
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:/users/sgbhat3/go/bin' >> ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
