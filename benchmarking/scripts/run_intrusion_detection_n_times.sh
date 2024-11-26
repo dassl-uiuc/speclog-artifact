34ef344ad03753b163dd47cb4494f30096040972
tshong <tshong@node0.tshong-230404.rasl-pg0.utah.cloudlab.us>
2024-11-26 08:49:21 -0700

    automate script

---
+++ benchmarking/scripts/run_intrusion_detection_n_times.sh
@@ -0,0 +1,10 @@
+n=$1
+for i in $(seq 1 $n)
+do
+    echo "Running $i-th time"
+    ./run.sh 6
+    mkdir -p ../../applications/vanilla_applications/intrusion_detection/analytics/intrusion_detection_run_$i
+    sudo cp -r ../../applications/vanilla_applications/intrusion_detection/data ../../applications/vanilla_applications/intrusion_detection/analytics/intrusion_detection_run_$i
+
+    sleep 10
+done
