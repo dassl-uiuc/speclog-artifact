e4ed7dec459f2934b8a77ca07000ced8e29f132b
Tony Hong <64179036+tonyhong007@users.noreply.github.com>
2024-12-05 15:15:06 -0600

    Update common.sh

--- benchmarking/scripts/common.sh
+++ benchmarking/scripts/common.sh
@@ -144,6 +144,11 @@ start_lagfix_clients() {
     ssh -o StrictHostKeyChecking=no -i $PASSLESS_ENTRY $1 "cd $benchmark_dir/scripts; $go_path run lagfix.go $2 $3 $4 $5 > ${LOGDIR}/client_$1_$3.log 2>&1" &
 }
 
+start_lagfix_e2e_clients() {
+    go_path="/usr/local/go/bin/go"
+    ssh -o StrictHostKeyChecking=no -i $PASSLESS_ENTRY $1 "cd $benchmark_dir/scripts; $go_path run lagfix_e2e.go $2 $3 $4 $5 > ${LOGDIR}/client_$1.log 2>&1" &
+}
+
 start_random_read_clients() {
     ssh -o StrictHostKeyChecking=no -i $PASSLESS_ENTRY $1 "cd $benchmark_dir/scripts; ./run_random_read_client.sh $2 $3 $1 $4 $5 $6 > ${LOGDIR}/client_$1.log 2>&1" &
 }
