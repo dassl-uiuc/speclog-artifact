645fb14c5a775f663fafb7e85485bb080fac24be
shreesha00 <shreeshagbhat@gmail.com>
2025-04-21 12:49:54 -0600

    added qc expt

---
+++ benchmarking/scripts/run/rq5/rq5.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+source ../../common.sh
+
+pushd $benchmark_dir/scripts
+
+# change params in files
+set_bool_variable_in_file \
+    ../../data/data_server.go \
+    "qcExpt" \
+    "true"
+
+set_bool_variable_in_file \
+    ../../order/order_server.go \
+    "qcExpt" \
+    "true"
+
+# run with lagfix enabled first 
+pushd $benchmark_dir/../
+go build 
+popd 
+
+sleep 5
+pushd $benchmark_dir/scripts
+
+# one shard involved in this expt
+num_shards=1
+
+## uncomment relavent code in data_server and order_server
+
+
+# new client joins when?
+new_client_join_time=30 
+computation_time=(800)
+
+# parameters
+runtime_secs=60
+
+cleanup_clients
+cleanup_servers
+clear_server_logs
+clear_client_logs
+
+start_order_nodes
+start_discovery
+
+# start shards
+start_data_nodes $num_shards
+
+# sleep for a bit befor starting clients
+sleep 5
+
+# start clients
+start_qc_e2e_clients ${client_nodes[0]} $runtime_secs 5 $new_client_join_time $computation_time $benchmark_dir/logs/ 
+echo "Waiting for clients to terminate"
+
+wait 
+
+cleanup_clients
+cleanup_servers
+
+collect_logs $num_shards
+
+# move logs to a different folder
+mkdir -p "$benchmark_dir/results/scalog_qc"
+mv $benchmark_dir/logs/* "$benchmark_dir/results/scalog_qc"
+
+
+popd
+
+# restore defaults and build
+
+set_bool_variable_in_file \
+    ../../data/data_server.go \
+    "qcExpt" \
+    "false"
+
+set_bool_variable_in_file \
+    ../../order/order_server.go \
+    "qcExpt" \
+    "false"
+
+pushd $benchmark_dir/../
+go build
+popd
+
+popd
