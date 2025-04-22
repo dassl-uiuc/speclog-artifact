66764d87fc72f123fd2b2eb724b039fb7969fea2
Tony Hong <64179036+tonyhong007@users.noreply.github.com>
2025-04-22 08:39:42 -0500

    Update run_transaction_analysis_n_times.sh

--- benchmarking/scripts/run_transaction_analysis_n_times.sh
+++ benchmarking/scripts/run_transaction_analysis_n_times.sh
@@ -3,6 +3,6 @@ for i in $(seq 1 $n)
 do
     echo "Running $i-th time"
     ./run.sh 7
-    mkdir -p ../../applications/vanilla_applications/transaction_analysis/analytics/transaction_analysis_run_$i
-    sudo cp -r ../../applications/vanilla_applications/transaction_analysis/data ../../applications/vanilla_applications/transaction_analysis/analytics/transaction_analysis_run_$i
+    mkdir -p ../../applications/vanilla_applications/transaction_analysis/speclog_$i
+    sudo cp -r ../../applications/vanilla_applications/transaction_analysis/data ../../applications/vanilla_applications/transaction_analysis/speclog_$
 done
