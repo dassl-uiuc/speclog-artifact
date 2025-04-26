b5ecf893d7691da7830f9bec0d4200eeb8711658
shreesha00 <shreeshagbhat@gmail.com>
2025-04-25 23:42:06 -0600

    adding a run all script

---
+++ benchmarking/scripts/run/run_all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Runs all the evaluation experiments for the paper
+# Usage: ./run_all.sh
+
+echo "Running all experiments..."   
+
+echo "Running RQ1 and RQ2..."
+pushd rq1\&2
+./run2shard.sh
+./run4shard.sh
+popd
+
+echo "Running RQ3..."
+pushd rq3
+./rq3.sh
+popd
+
+echo "Running RQ4..."
+pushd rq4
+./rq4.sh
+popd
+
+echo "Running RQ5..."
+pushd rq5
+./rq5.sh
+popd
+
+echo "Running RQ6..."
+pushd rq6
+./rq6.sh
+popd
+
+echo "Running RQ7..."
+pushd rq7
+./rq7.sh
+popd
+
+# TODO RQ9
+
+echo "Running RQ10..."
+pushd rq10
+./rq10.sh
+popd
+
+echo "Done!"
