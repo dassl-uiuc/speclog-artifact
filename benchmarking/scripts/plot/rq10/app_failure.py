1043052cd1b8936fd1706732f38564499f6189f2
shreesha00 <shreeshagbhat@gmail.com>
2025-05-16 12:46:30 -0600

    seperated app failure plots

--- benchmarking/scripts/plot/rq10/app_failure.py
+++ benchmarking/scripts/plot/rq10/app_failure.py
@@ -30,7 +30,7 @@ diff = float(shard_failure) - float(replica_failure)
 zoomstart = int(float(shard_failure))
 zoomend = zoomstart + 1
 
-gnuplot_lat = rf"""
+gnuplot_lat_shard = rf"""
 #! /bin/bash
 #run this script to generate the lat vs thrpt graphs for c and w
 
@@ -40,7 +40,7 @@ gnuplot_lat = rf"""
 
 (cat <<EOF
 	set terminal postscript eps enhanced color size 4, 1.4 font "Times-new-roman,19"
-	set output "fail_e2e.eps"
+	set output "fail_e2e_shard.eps"
 	set xlabel "Time (s)" font "Times-new-roman,22" offset 0,0.4,0
 	set ylabel "E2E Latency (ms)" font "Times-new-roman,22" offset 1,-0.2,0
 	set tmargin 0.5
@@ -58,18 +58,16 @@ gnuplot_lat = rf"""
 	set style function linespoints
 	set trange [0:100]
 	set parametric
-	plot {shard_failure},t title "Shard/Replica Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
+	plot {shard_failure},t title "Shard Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
 		 {failure_detected},t title "Detected" dt 1 lw 5 ps 0 lc rgb 'coral',\
 		 {view_change},t title "View change" dt 2 lw 5 ps 0  lc rgb 'black',\
-	     "e2elat" using 1:2 title 'E2E Latency' with lines lc rgb '#009988' dashtype 1 lw 5, \
-         "e2elat_mask" using (\$1+{diff}):2 title 'Belfast SIF' with lines lc rgb '#FFD689' dashtype 1 lw 5
-		
+	     "e2elat" using 1:2 title 'E2E Latency' with lines lc rgb '#009988' dashtype 1 lw 5
 		
 EOF
 ) | gnuplot -persist
 """
 
-gnuplot_tput = rf"""
+gnuplot_tput_shard = rf"""
 #! /bin/bash
 #run this script to generate the lat vs thrpt graphs for c and w
 
@@ -79,7 +77,7 @@ gnuplot_tput = rf"""
 
 (cat <<EOF
 	set terminal postscript eps enhanced color size 4, 1.4 font "Times-new-roman,19"
-	set output "fail_tput.eps"
+	set output "fail_tput_shard.eps"
 	set xlabel "Time (s)" font "Times-new-roman,22" offset 0,0.4,0
 	set ylabel "Throughput (KOps/s)" font "Times-new-roman,22" offset 1,-1,0
 	set tmargin 0.5
@@ -100,12 +98,82 @@ gnuplot_tput = rf"""
 
 	set trange [0:20]
 	set parametric
-	plot {shard_failure},t title "Shard/Replica Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
+	plot {shard_failure},t title "Shard Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
 		 {failure_detected},t title "Detected" dt 1 lw 5 ps 0 lc rgb 'coral',\
 		 {view_change},t title "View change" dt 2 lw 5 ps 0  lc rgb 'black',\
-		 "tput" using 1:(\$2/1000) title 'Throughput' with lines lc rgb '#009988'  dashtype 1 lw 5, \
-         "tput_mask" using (\$1+{diff}):(\$2/1000) title 'Belfast SIF' with lines lc rgb '#FFD689' dashtype 1 lw 5
+		 "tput" using 1:(\$2/1000) title 'Throughput' with lines lc rgb '#009988'  dashtype 1 lw 5
+EOF
+) | gnuplot -persist
+"""
+
+gnuplot_lat_replica = rf"""
+#! /bin/bash
+#run this script to generate the lat vs thrpt graphs for c and w
+
+#./plot_lat_thrpt.sh ../paper_results/wc-2 sync w 2;
+#./plot_lat_thrpt.sh ../paper_results/wc-2 async w 2;
+#./plot_lat_thrpt.sh ../paper_results/wc-2 sync c 2;
+
+(cat <<EOF
+	set terminal postscript eps enhanced color size 4, 1.4 font "Times-new-roman,19"
+	set output "fail_e2e_replica.eps"
+	set xlabel "Time (s)" font "Times-new-roman,22" offset 0,0.4,0
+	set ylabel "E2E Latency (ms)" font "Times-new-roman,22" offset 1,-0.2,0
+	set tmargin 0.5
+	set lmargin 6.3
+	set encoding utf8
+	set rmargin 18
+	set bmargin 3
+	set yrange [0:72]
+	#set xrange [0:17]
+	set xrange [{zoomstart}:{zoomend}]
+	set tics scale 0.4
+	#set ytics 0,200,800 scale 0.4
+	set xtics 0.2 scale 0.4
+	set key outside right center font "Times-new-roman,18" samplen 2 maxrows 5 width -2
+	set style function linespoints
+	set trange [0:100]
+	set parametric
+	plot {replica_failure}+{diff},t title "Replica Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
+	     "e2elat_mask" using (\$1+{diff}):2 title 'E2E Latency' with lines lc rgb '#009988' dashtype 1 lw 5
 		
+EOF
+) | gnuplot -persist
+"""
+
+gnuplot_tput_replica = rf"""
+#! /bin/bash
+#run this script to generate the lat vs thrpt graphs for c and w
+
+#./plot_lat_thrpt.sh ../paper_results/wc-2 sync w 2;
+#./plot_lat_thrpt.sh ../paper_results/wc-2 async w 2;
+#./plot_lat_thrpt.sh ../paper_results/wc-2 sync c 2;
+
+(cat <<EOF
+	set terminal postscript eps enhanced color size 4, 1.4 font "Times-new-roman,19"
+	set output "fail_tput_replica.eps"
+	set xlabel "Time (s)" font "Times-new-roman,22" offset 0,0.4,0
+	set ylabel "Throughput (KOps/s)" font "Times-new-roman,22" offset 1,-1,0
+	set tmargin 0.5
+	set lmargin 5.5
+	set encoding utf8
+	set rmargin 18.3
+	set bmargin 3
+	set yrange [0:18]
+	set xrange [{zoomstart}:{zoomend}]
+	#set y2tics nomirror
+	#set ytics nomirror
+	#set y2range[1:]
+	set tics scale 0.4
+	set ytics 4 scale 0.4
+	set xtics 0.2 scale 0.4
+	set key outside right center font "Times-new-roman,18" samplen 3 maxrows 5 width -2
+	set style function linespoints
+
+	set trange [0:20]
+	set parametric
+	plot {replica_failure}+{diff},t title "Replica Fail" dt 3 lw 5 ps 0 lc rgb 'gray',\
+		 "tput_mask" using (\$1+{diff}):(\$2/1000) title 'Throughput' with lines lc rgb '#009988'  dashtype 1 lw 5
 EOF
 ) | gnuplot -persist
 """
@@ -142,13 +210,20 @@ EOF
 ) | gnuplot -persist
 """
 
-subprocess.run(["bash"], input=gnuplot_lat, text=True)
-subprocess.run(["bash"], input=gnuplot_tput, text=True)
+subprocess.run(["bash"], input=gnuplot_lat_shard, text=True)
+subprocess.run(["bash"], input=gnuplot_tput_shard, text=True)
+subprocess.run(["bash"], input=gnuplot_lat_replica, text=True)
+subprocess.run(["bash"], input=gnuplot_tput_replica, text=True)
 subprocess.run(["bash"], input=gnuplot_breakdown, text=True)
-subprocess.run(["bash"], input="epstopdf fail_e2e.eps", text=True)
-subprocess.run(["bash"], input="epstopdf fail_tput.eps", text=True)
+subprocess.run(["bash"], input="epstopdf fail_e2e_shard.eps", text=True)
+subprocess.run(["bash"], input="epstopdf fail_tput_shard.eps", text=True)
+subprocess.run(["bash"], input="epstopdf fail_e2e_replica.eps", text=True)
+subprocess.run(["bash"], input="epstopdf fail_tput_replica.eps", text=True)
 subprocess.run(["bash"], input="epstopdf breakdown.eps", text=True)
 subprocess.run(["bash"], input="rm breakdown.eps breakdown", text=True)
-subprocess.run(["bash"], input="rm fail_e2e.eps fail_tput.eps e2elat tput e2elat_mask tput_mask", text=True)
-subprocess.run(["bash"], input="mv fail_e2e.pdf 14a.pdf", text=True)
-subprocess.run(["bash"], input="mv fail_tput.pdf 14b.pdf", text=True)
+subprocess.run(["bash"], input="rm fail_e2e_shard.eps fail_tput_shard.eps fail_e2e_replica.eps fail_tput_replica.eps e2elat tput e2elat_mask tput_mask", text=True)
+subprocess.run(["bash"], input="mv fail_e2e_shard.pdf app_failure_shard_e2e.pdf", text=True)
+subprocess.run(["bash"], input="mv fail_tput_shard.pdf app_failure_shard_tput.pdf", text=True)
+subprocess.run(["bash"], input="mv fail_e2e_replica.pdf app_failure_replica_e2e.pdf", text=True)
+subprocess.run(["bash"], input="mv fail_tput_replica.pdf app_failure_replica_tput.pdf", text=True)
+subprocess.run(["bash"], input="mv breakdown.pdf app_failure_breakdown.pdf", text=True)
