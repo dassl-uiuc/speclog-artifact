e2742ee5176941c2a46f9a64dc3f7aff9b440a60
shreesha00 <shreeshagbhat@gmail.com>
2025-06-02 00:35:40 -0600

    emulation scripts

---
+++ benchmarking/scripts/run/rq8/rq8.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ../../common.sh
+pushd $benchmark_dir/scripts/
+
+# num_shards=(5 10 15 20 25 30 35 40)
+num_shards=(6 8 10 12 16 20)
+rate=10000
+for num_shard in "${num_shards[@]}"
+do
+    echo "Running emulation for $num_shard shards"
+    cleanup_clients
+    cleanup_servers
+    clear_server_logs
+    clear_client_logs
+
+    start_order_nodes
+    start_discovery
+    start_data_nodes ${num_shard} ${rate}
+
+    sleep 140
+
+    cleanup_clients
+    cleanup_servers
+    collect_logs ${num_shard}
+
+    # move logs to a different folder
+    mkdir -p "$results_dir/scalog_em/emulation_$num_shard"
+    mv $benchmark_dir/logs/* "$results_dir/scalog_em/emulation_$num_shard"
+done 
+
+popd
